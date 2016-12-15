97d44532b83f933c0520a91da740f5d7127d7d2d
Jim Hinckley <jim@jhwm.net>
2016-12-15 22:12:14 +0000

    Enabled form to add new records to database and re-direct submitter to Home page.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,22 @@
 class PlacesController < ApplicationController
 
-  def index
+  def index   # Displays 'place' records on 'root' page (10 per page) 
     @places = Place.all.page(params[:page]).per(10)
   end
 
-  def new
+  def new   # Creates a new database record based on visitor's entry
     @place = Place.new
+  end
+
+  def create   
+    Place.create(place_params)   # Sends visitor's form submission to the database.
+    redirect_to root_path     # Redirects visitor to Home page after submitting form.
+  end
+
+  private
+
+  def place_params   # Pulls submitted name, description and address values from the form.
+    params.require(:place).permit(:name, :description, :address)
   end
 
 end
