0609b1174fd859b9dc9277b0820116f19e9d64da
Jim Hinckley <jim@jhwm.net>
2016-12-17 18:48:53 +0000

    Connected Users to Places (limited new-place creation to logged-in users)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,16 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
-  def index   # Displays 'place' records on 'root' page (10 per page) 
+  def index   # Displays 'place' records (10 per page) on 'root' page(s)
     @places = Place.all.page(params[:page]).per(10)
   end
 
-  def new   # Creates a new database record based on visitor's entry
+  def new   # Creates a new database record based on data the visitor submitted
     @place = Place.new
   end
 
   def create   
-    Place.create(place_params)   # Sends visitor's form submission to the database.
+    current_user.places.create(place_params)   # Sends visitor's form submission to the database.
     redirect_to root_path     # Redirects visitor to Home page after submitting form.
   end
 
