834fec04aeb37d9885c6053e6e6adbfd38decd17
Jim Hinckley <jim@jhwm.net>
2016-12-20 15:57:10 +0000

    Enabled logged-in user to edit details of places she created.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,6 +18,10 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params   # Pulls submitted name, description and address values from the form.
